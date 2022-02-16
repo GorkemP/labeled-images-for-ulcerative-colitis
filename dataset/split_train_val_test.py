19eb908cbd9454bb2caaa576567f2dfab2a4234d
GorkemP <polatgorkem@gmail.com>
2022-02-16 10:35:01 +0300

    make patient-level splitting

--- dataset/split_train_val_test.py
+++ dataset/split_train_val_test.py
@@ -2,64 +2,129 @@ import glob
 import os
 from distutils.dir_util import copy_tree
 import numpy as np
-from shutil import move
-import math
+from math import ceil
+from random import shuffle
 import shutil
 import random
+import argparse
 
 np.random.seed(35)
 random.seed(35)
 
-root_dir = "/home/ws2080/Desktop/data/ucmayo4_gan"
-original_folder = "final_annotations"
-test_ratio = 15
-val_ratio = 15
-# So, train ratio = 100 - (test_ratio + val_ratio)
+parser = argparse.ArgumentParser(description="This script generates train-val-test sets by splitting \n"
+                                             "original dataset (patient_based_classified_images) in \n"
+                                             "patient-level. Due to patient-level splitting, train-val-test \n"
+                                             "sets ratios may not be exactly as it is given in ratios.\n")
 
-# Delete if there are folders named train, test, val
-if os.path.isdir(os.path.join(root_dir, "train")):
-    shutil.rmtree(os.path.join(root_dir, "train"))
-if os.path.isdir(os.path.join(root_dir, "test")):
-    shutil.rmtree(os.path.join(root_dir, "test"))
-if os.path.isdir(os.path.join(root_dir, "val")):
-    shutil.rmtree(os.path.join(root_dir, "val"))
+parser.add_argument("--target_dir", type=str, required=True,
+                    help="train, val, test folders will be created in this folder.")
+parser.add_argument("--published_folder_path", type=str, required=True,
+                    help="path of patient_based_classified_images folder.")
+parser.add_argument("--test_set_ratio", type=float, default=0.2,  help="ratio of the test set.")
+parser.add_argument("--val_set_ratio", type=float, default=0.2,  help="ratio of the val set.")
+parser.add_argument("--error_ratio", type=float, default=0.15,
+                    help="tolerance level for test and validation set ratios.")
 
-subFolders = glob.glob(os.path.join(root_dir, original_folder, "*"))
-subFolders.sort()
+args = parser.parse_args()
 
-# At first, train folder is copy of the original folder. Test and validation folders
-# will take images from the train folder
+target_dir = args.target_dir
+published_folder_path = args.published_folder_path
+test_set_ratio = float(args.test_set_ratio)
+val_set_ratio = float(args.val_set_ratio)
+error_ratio = float(args.error_ratio)
 
-# Create train folder
-copy_tree(os.path.join(root_dir, original_folder), os.path.join(root_dir, "train"))
-# Create val folder
-os.mkdir(os.path.join(root_dir, "val"))
-# Create test folder
-os.mkdir(os.path.join(root_dir, "test"))
+all_folders = os.listdir(published_folder_path)
+folder_class_names = sorted(os.listdir(os.path.join(published_folder_path, all_folders[0])))
 
-for folder in subFolders:
-    folder_name = folder.split("/")[-1]
-    image_paths = glob.glob(os.path.join(folder, "*"))
-    print("Total images in folder " + folder_name + ": " + str(len(image_paths)))
+pass_state = False
 
-    random_ordering = np.random.permutation(len(image_paths))
+while not pass_state:
+    pass_state = True
 
-    # move from train to test folder
-    test_size = math.ceil(len(image_paths) * test_ratio / 100)
-    os.mkdir(os.path.join(root_dir, "test", folder_name))
+    # Delete if there are folders named train, test, val in target directory
+    if os.path.isdir(os.path.join(target_dir, "train")):
+        shutil.rmtree(os.path.join(target_dir, "train"))
+    if os.path.isdir(os.path.join(target_dir, "test")):
+        shutil.rmtree(os.path.join(target_dir, "test"))
+    if os.path.isdir(os.path.join(target_dir, "val")):
+        shutil.rmtree(os.path.join(target_dir, "val"))
 
-    for i in range(test_size):
-        file_name = image_paths[random_ordering[i]].split("/")[-1]
-        move(os.path.join(root_dir, "train", folder_name, file_name), os.path.join(root_dir, "test", folder_name,
-                                                                                   file_name))
-    print("----> Test Set: For folder " + folder_name + ", " + str(test_size) + " files are copied")
+    shuffle(all_folders)
 
-    # move from train to val folder
-    val_size = math.ceil(len(image_paths) * val_ratio / 100)
-    os.mkdir(os.path.join(root_dir, "val", folder_name))
+    val_set_folder_size = ceil(val_set_ratio * len(all_folders))
+    test_set_folder_size = ceil(test_set_ratio * len(all_folders))
 
-    for i in range(test_size, test_size + val_size):
-        file_name = image_paths[random_ordering[i]].split("/")[-1]
-        move(os.path.join(root_dir, "train", folder_name, file_name), os.path.join(root_dir, "val", folder_name,
-                                                                                   file_name))
-    print("----> Validation Set: For folder " + folder_name + ", " + str(val_size) + " files are copied")
+    val_folders = all_folders[0:val_set_folder_size]
+    test_folders = all_folders[val_set_folder_size:(val_set_folder_size + test_set_folder_size)]
+    train_folders = all_folders[(val_set_folder_size + test_set_folder_size):]
+
+    # Create train folder
+    target_train_path = os.path.join(target_dir, "train")
+    os.mkdir(target_train_path)
+    for class_name in folder_class_names:
+        os.mkdir(os.path.join(target_train_path, class_name))
+
+    for train_folder in train_folders:
+        for class_folder in os.scandir(os.path.join(published_folder_path, train_folder)):
+            target_directory = os.path.join(target_train_path, class_folder.name)
+            copy_tree(class_folder.path, target_directory)
+
+    # Create val folder
+    target_val_path = os.path.join(target_dir, "val")
+    os.mkdir(target_val_path)
+    for class_name in folder_class_names:
+        os.mkdir(os.path.join(target_val_path, class_name))
+
+    for val_folder in val_folders:
+        for class_folder in os.scandir(os.path.join(published_folder_path, val_folder)):
+            target_directory = os.path.join(target_val_path, class_folder.name)
+            copy_tree(class_folder.path, target_directory)
+
+    # Create test folder
+    target_test_path = os.path.join(target_dir, "test")
+    os.mkdir(target_test_path)
+    for class_name in folder_class_names:
+        os.mkdir(os.path.join(target_test_path, class_name))
+
+    for test_folder in test_folders:
+        for class_folder in os.scandir(os.path.join(published_folder_path, test_folder)):
+            target_directory = os.path.join(target_test_path, class_folder.name)
+            copy_tree(class_folder.path, target_directory)
+
+    print("\nChecking splitting ratios:")
+    for class_name in folder_class_names:
+        print(class_name)
+        train_size = len(glob.glob(os.path.join(target_train_path, class_name, "*.bmp")))
+        val_size = len(glob.glob(os.path.join(target_val_path, class_name, "*.bmp")))
+        test_size = len(glob.glob(os.path.join(target_test_path, class_name, "*.bmp")))
+        total = train_size + val_size + test_size
+
+        if ((test_size / total) < test_set_ratio * (1 - error_ratio)) or (
+                (test_size / total) > test_set_ratio * (1 + error_ratio)) \
+                or ((val_size / total) < val_set_ratio * (1 - error_ratio)) or (
+                (val_size / total) > val_set_ratio * (1 + error_ratio)):
+            print("test size: {:4.3f}".format(test_size / total))
+            print("val size: {:4.3f}".format(val_size / total))
+            print("Unbalanced distribution for class " + class_name + ". Recalculating!")
+            pass_state = False
+            break
+
+    if pass_state:
+        print("Successful distribution!")
+
+print("\nClass ratios for test and val set:")
+target_train_path = os.path.join(target_dir, "train")
+target_val_path = os.path.join(target_dir, "val")
+target_test_path = os.path.join(target_dir, "test")
+
+total_classes = 0
+for class_name in folder_class_names:
+    train_size = len(glob.glob(os.path.join(target_train_path, class_name, "*.bmp")))
+    val_size = len(glob.glob(os.path.join(target_val_path, class_name, "*.bmp")))
+    test_size = len(glob.glob(os.path.join(target_test_path, class_name, "*.bmp")))
+    total = train_size + val_size + test_size
+    total_classes += total
+
+    print(class_name + ": " + " test set ratio: {:4.3f}".format(test_size / total) + " val set ratio: {:4.3f}".format(
+            val_size / total) + " => total: " + str(total))
+print("All images: " + str(total_classes))
