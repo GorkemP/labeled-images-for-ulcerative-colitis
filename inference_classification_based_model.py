26e2fb37e779d0321579c81a07d6d4484e367abe
GorkemP <polatgorkem@gmail.com>
2022-02-16 22:11:36 +0300

    add argparse

--- inference_classification_based_model.py
+++ inference_classification_based_model.py
@@ -3,7 +3,6 @@
 
 import torch
 import torchvision.transforms as transforms
-
 from dataset.ucmayo4 import UCMayo4
 from utils.metrics import get_mean_sensitivity_specificity
 from utils.provider import get_test_results_classification, get_dataset_mean_and_std
@@ -13,16 +12,29 @@ from sklearn.metrics import confusion_matrix, classification_report, cohen_kappa
 from utils import provider
 from itertools import cycle
 import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser(description="Arguments for the inference.")
+
+parser.add_argument("--train_dir", type=str, required=True, help="path to training set.")
+parser.add_argument("--test_dir", type=str, required=True, help="path to validation set.")
+parser.add_argument("--model_name", type=str, required=True,
+                    choices=["ResNet18", "ResNet50", "VGG16_bn", "DenseNet121", "Inception_v3", "MobileNet_v3_large"],
+                    help="Name of the CNN architecture.")
+parser.add_argument("--checkpoint", type=str, required=True, help="path to checkpoint file (pretrained weights).")
+
+
+args = parser.parse_args()
 
 num_worker = 4
-state_dict_name = 'weights/best_ResNet50.pth.tar'
-model_name = "ResNet50"
+state_dict_name = args.checkpoint
+model_name = args.model_name
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("device: ", device)
 
-train_dir = "/home/ws2080/Desktop/data/ucmayo4/cross_validation_folds_splitted_test/fold_0/train"
-test_dir = "/home/ws2080/Desktop/data/ucmayo4/cross_validation_folds_splitted_test/test"
+train_dir = args.train_dir
+test_dir = args.test_dir
 
 channel_means, channel_stds = get_dataset_mean_and_std(train_dir)
 normalize = transforms.Normalize(mean=channel_means,
@@ -44,8 +56,10 @@ model = provider.initialize_model(model_name, False, 4)
 model.load_state_dict(torch.load(state_dict_name))
 model.to(device)
 
+print("inference started...")
 y_true, y_probs, y_pred, r_true, r_probs, r_pred = get_test_results_classification(model, test_loader, device, True,
                                                                                    [2, 3])
+print("inference finished!")
 
 prf1 = precision_recall_fscore_support(y_true, y_pred)
 
@@ -70,7 +84,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('ROC Eğrisi - ' + "Bütün Mayo Sınıfları")
+plt.title('ROC Curve- ' + "All Mayo Classes")
 plt.legend(loc="lower right")
 plt.show()
 
@@ -83,8 +97,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-# plt.title('ROC for Remission State - ' + state_dict_name)
-plt.title('ROC Eğrisi - ' + "Remisyon")
+plt.title('ROC Curve - ' + "Remission")
 plt.legend(loc="lower right")
 plt.show()
 
@@ -99,7 +112,7 @@ mean_sensitivity, mean_specificity = get_mean_sensitivity_specificity(y_true, y_
 
 print("\n------------ Classification Report (All) ------------\n")
 print("Average Accuracy: {:0.4f}".format(accuracy))
-print("4-class QWK kappa score: " + str(QWK_kappa_score))
+print("QWK score: " + str(QWK_kappa_score))
 print("Average Sensitivity: {:0.4f}".format(mean_sensitivity))
 print("Average Specificity: {:0.4f}".format(mean_specificity))
 print("mean AUC score: " + str(AUC) + " / " + str(sum(roc_auc.values()) / 4))
--- inference_regression_based_model.py
+++ inference_regression_based_model.py
@@ -3,23 +3,34 @@
 
 import torch
 import torchvision.transforms as transforms
-from torch.utils.tensorboard import SummaryWriter
-
 from dataset.ucmayo4 import UCMayo4
 from utils.metrics import get_mean_sensitivity_specificity
 from utils.provider import get_test_results_regression, get_dataset_mean_and_std
 from sklearn.metrics import confusion_matrix, classification_report, cohen_kappa_score, accuracy_score
 from utils import provider
+import argparse
+
+parser = argparse.ArgumentParser(description="Arguments for the inference.")
+
+parser.add_argument("--train_dir", type=str, required=True, help="path to training set.")
+parser.add_argument("--test_dir", type=str, required=True, help="path to validation set.")
+parser.add_argument("--model_name", type=str, required=True,
+                    choices=["ResNet18", "ResNet50", "VGG16_bn", "DenseNet121", "Inception_v3", "MobileNet_v3_large"],
+                    help="Name of the CNN architecture.")
+parser.add_argument("--checkpoint", type=str, required=True, help="path to checkpoint file (pretrained weights).")
+
+
+args = parser.parse_args()
 
 num_worker = 4
-state_dict_name = 'weights/best_R_ResNet18.pth.tar'
-model_name = "ResNet50"
+state_dict_name = args.checkpoint
+model_name = args.model_name
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("device: ", device)
 
-train_dir = "/home/ws2080/Desktop/data/ucmayo4/cross_validation_folds_splitted_test/fold_0/train"
-test_dir = "/home/ws2080/Desktop/data/ucmayo4/test"
+train_dir = args.train_dir
+test_dir = args.test_dir
 
 channel_means, channel_stds = get_dataset_mean_and_std(train_dir)
 normalize = transforms.Normalize(mean=channel_means,
@@ -52,7 +63,7 @@ mean_sensitivity, mean_specificity = get_mean_sensitivity_specificity(y_true, y_
 
 print("------------ Classification Report (All) ------------\n")
 print("Average Accuracy: {:0.4f}".format(accuracy))
-print("4-class kappa score: " + str(QWK_kappa_score))
+print("QWK score: " + str(QWK_kappa_score))
 print("Average Sensitivity: {:0.4f}".format(mean_sensitivity))
 print("Average Specificity: {:0.4f}".format(mean_specificity))
 
