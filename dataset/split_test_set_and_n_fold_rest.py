41cbe69dc713fd762fb26968dc54d2e008c25204
GorkemP <polatgorkem@gmail.com>
2022-02-16 10:34:45 +0300

    add argparse arguments

--- dataset/split_test_set_and_n_fold_rest.py
+++ dataset/split_test_set_and_n_fold_rest.py
@@ -9,22 +9,41 @@ import shutil
 from distutils.dir_util import copy_tree
 from math import ceil
 import numpy as np
+import argparse
 
-# fix seed in order to get same folds for reproducibility
+# fix seed to get same folds for reproducibility
 random.seed(35)
 np.random.seed(35)
 
-CV_folder_path = "/home/ws2080/Desktop/data/ucmayo4/cross_validation_folds_splitted_test"
-published_folder_path = "/home/ws2080/Desktop/data/ucmayo4/published_dataset_final_annotations"
+parser = argparse.ArgumentParser(description="This script generates n-folds for cross-validation. \n"
+                                             "First, it forms a test set by given test_set_ratio. \n"
+                                             "Then, it creates n train-val folds. \n"
+                                             "validation and test sets ratios may not be exactly as \n"
+                                             "it is given in ratios due to patient-level splitting\n")
 
-fold_num = 10
+parser.add_argument("--CV_folder_path", type=str, required=True,
+                    help="CV folds and test set folder will be created in this path.")
+parser.add_argument("--published_folder_path", type=str, required=True,
+                    help="path of patient_based_classified_images folder.")
+parser.add_argument("--fold_num", type=int, default=10, help="Number of folds.")
+parser.add_argument("--test_set_ratio", type=float, default=0.2, help="ratio of the test set.")
+parser.add_argument("--test_set_error_ratio", type=float, default=0.1, help="tolerance of the test set ratio.")
+parser.add_argument("--error_ratio", type=float, default=0.15,
+                    help="tolerance level for validation set ratios.")
+
+args = parser.parse_args()
+
+CV_folder_path = args.CV_folder_path
+published_folder_path = args.published_folder_path
+
+fold_num = args.fold_num
 fold_folder_prefix = "fold"
 all_folders = os.listdir(published_folder_path)
 folder_class_names = sorted(os.listdir(os.path.join(published_folder_path, all_folders[0])))
 
-error_ratio = 0.15
-test_set_error_ratio = 0.1
-test_set_ratio = 0.15
+error_ratio = args.error_ratio
+test_set_error_ratio = args.test_set_error_ratio
+test_set_ratio = args.test_set_ratio
 val_set_ratio = 1. / fold_num
 
 if os.path.isdir(CV_folder_path):
@@ -69,6 +88,7 @@ while not test_set_pass:
             target_directory = os.path.join(target_test_path, class_folder.name)
             copy_tree(class_folder.path, target_directory)
 
+    print("\nChecking test set ratios:")
     for class_name in folder_class_names:
         test_size = len(glob.glob(os.path.join(target_test_path, class_name, "*.bmp")))
         total = class_sizes[class_name]
@@ -124,6 +144,7 @@ while i < fold_num:
             target_directory = os.path.join(target_val_path, class_folder.name)
             copy_tree(class_folder.path, target_directory)
 
+    print("\nChecking splitting ratios for folds:")
     for class_name in folder_class_names:
         train_size = len(glob.glob(os.path.join(target_train_path, class_name, "*.bmp")))
         val_size = len(glob.glob(os.path.join(target_val_path, class_name, "*.bmp")))
@@ -132,7 +153,7 @@ while i < fold_num:
 
         if ((val_size / total) < (val_set_ratio * (1 - test_size / total) * (1 - error_ratio))) or (
                 (val_size / total) > (val_set_ratio * (1 - test_size / total) * (1 + error_ratio))):
-            print("unbalanced distribution in fold " + str(i) + " for class " + class_name + ". Recalculating!")
+            print("Unbalanced distribution in fold " + str(i) + " for class " + class_name + ". Recalculating!")
             print("val size: {:3.1f}".format((val_size / total) * 100))
             pass_state = False
             break
@@ -141,6 +162,7 @@ while i < fold_num:
         print("Successful distribution in fold " + str(i) + ". Advancing to next fold.")
         i += 1
 
+print("\nClass ratios for val set:")
 for fold in os.scandir(CV_folder_path):
     if fold.name.startswith("fold"):
         print("\n" + fold.name)
@@ -155,6 +177,7 @@ for fold in os.scandir(CV_folder_path):
             total_classes += total
 
             print(
-                    class_name + ": " + " val size: {:3.1f}".format((val_size / total) * 100) + " => total: " + str(
+                    class_name + ": " + " val set ratio: {:3.1f}".format(
+                        (val_size / total) * 100) + " => total: " + str(
                             total))
         print("All images: " + str(total_classes))
